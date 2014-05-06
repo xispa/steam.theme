126aad5183dc90e62a712d9a02b39f0863d021b3
Jordi Puiggené <jordi.puiggene@gmail.com>
2014-05-06 19:18:56 +0200

    Added support for portal-alert

--- steam/theme/resources/js/main.js
+++ steam/theme/resources/js/main.js
@@ -41,4 +41,30 @@ $(document).ready(function(){
 		$("#column-two").children().length == 0) {
 		$("#column-content").css("margin", "0px 0px 0px 0px");
 	}
+	
+	/*var $window = $(window),
+    $body = $('body'),
+    $headertop = $('#header-top'),
+    $mainmenu = $('#mainmenu'),
+    $menusubbar = $('#menusubbar')
+    $editbar = $('#editbar');
+	
+    menuheight         = $mainmenu.outerHeight();
+	menubottomposition = $mainmenu.offset().top + menuheight;
+	paddingtop         = $headertop.outerHeight() + $header.outerHeight() - menuheight + 12;
+	contentposition    = $headertop.outerHeight() + $header.outerHeight() + menuheight + $contenthead.outerHeight();
+	
+	$window.scroll(function() {
+        $mainmenu.removeClass('pinned');
+        $mainmenu.removeClass('pinned-marged');
+        if ($window.scrollTop() > contentposition) {
+          $mainmenu.addClass('pinned-marged');
+          $body.css('paddingTop', paddingtop +'px');
+        } else if ($window.scrollTop() > menubottomposition) {
+          $mainmenu.addClass('pinned');
+          $body.css('paddingTop', paddingtop +'px');
+        } else {
+          $body.css('paddingTop', '');
+        }
+    });*/
 })
